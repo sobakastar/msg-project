2a14883905c2e6b01c655a0bd2dec1a642666e9b
sobakastar <dr.gris@yandex.ru>
2022-04-06 23:00:12 +0300

    fixing the changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@
 import "./App.css";
 import React from "react";
 import { Message } from "./components/Message/Message";
-// import { Counter } from "./components/Example/Example";
-import { useState } from "react";
+import { Counter } from "./components/Example/Example";
 import { Form } from "./components/Form/Form";
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { MessageList } from "./components/MessageList/MessageList";
 import { AUTHORS } from "./utils/constants";
 
@@ -14,6 +13,9 @@ import { AUTHORS } from "./utils/constants";
 function App() {
   //const [messageList, setMessageList] = useState([]);
   const [messages, setMessages] = useState([]);
+
+  const timeout = useRef();
+  const wrapperRef = useRef();
 
   const addMessage = (newMsg) => {
     setMessages([...messages, newMsg]);
@@ -24,33 +26,42 @@ function App() {
       {
         author: AUTHORS.human,
         text, // text: text
+        id: `msg-${Date.now()}`,
       },
       1000 // не работает ?
     );
   };
 
   useEffect(() => {
-    let timeout;
     if (messages[messages.length - 1]?.author === AUTHORS.human) {
-      timeout = setTimeout(() => {
-        addMessage({ author: AUTHORS.robot, text: "hello" });
+      timeout.current = setTimeout(() => {
+        addMessage({
+          author: AUTHORS.robot,
+          text: "hello",
+          id: `msg-${Date.now()}`,
+        });
       });
     }
 
     return () => {
-      clearTimeout(timeout);
+      clearTimeout(timeout.current);
     };
   }, [messages]);
 
+  const handleScroll = () => {
+    wrapperRef.current?.scrollTo({ x: 0, y: 0 });
+  };
+
   return (
-    <div className="App">
+    <div className="App" ref={wrapperRef}>
       {/* <Counter randomNumber={rand}/> */}
       {/*{messageList.map((msg) => (
         <Message text={msg.text} author={msg.author} />
       ))} */}
-      
+
       <MessageList messages={messages} />
       <Form onSubmit={sendMessage} />
+      <button onClick={handleScroll}>scroll</button>
     </div>
   );
 }
